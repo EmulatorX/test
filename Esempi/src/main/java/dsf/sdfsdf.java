c70342860fea33a4c96b54a0450391559cac5974
EmulatorX <massimo.ghianda@gmail.com>
2023-07-21 14:47:26 +0200

    Primo Commit

---
+++ Esempi/src/main/java/dsf/sdfsdf.java
@@ -0,0 +1,35 @@
+package dsf;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionActivationListener;
+import javax.servlet.http.HttpSessionEvent;
+
+/**
+ * Application Lifecycle Listener implementation class sdfsdf
+ *
+ */
+@WebListener
+public class sdfsdf implements HttpSessionActivationListener {
+
+    /**
+     * Default constructor. 
+     */
+    public sdfsdf() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionActivationListener#sessionDidActivate(HttpSessionEvent)
+     */
+    public void sessionDidActivate(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionActivationListener#sessionWillPassivate(HttpSessionEvent)
+     */
+    public void sessionWillPassivate(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
